aca34be4b731e8caf510f72e0a1d8065e347deb8
ssalasky <samsalasky@hotmail.com>
2017-12-12 21:17:42 -0700

    deploying to heroku

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -14,7 +14,7 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.profpic = "../../assets/images/profile.jpg"
+    this.profpic = "./assets/images/profile.jpg"
     this.personal = 'I was born and raised in Colorado. I grew up playing hockey, soccer and snowboarding.' +
       'I have been to over 10 different coutries and look to add a new country every year.' +
       'In 2017 I set the goal to attend 50 concerts and will end the year with four concerts on each of the last four nights of this year to hit my goal.'
